3c20ec5e4109a6e1011eb5ffccb492852a832dfc
franco22bellido <franco.bellido22@gmail.com>
2023-01-12 19:32:58 -0300

    creando authController

--- src/injecting/injections.ts
+++ src/injecting/injections.ts
@@ -1,9 +1,22 @@
 import {BooksController} from '../controllers/booksController';
+import AuthController from '../controllers/authController';
 
 //injections:
-import implBookOne from '../dao/bookImpls/implBookOne';
 import IbookDao from "../dao/IBookDao";
+import implBookOne from '../dao/bookImpls/implBookOne';
 
 /*injectando a booksController */
 const injection:IbookDao = new implBookOne();
 export const booksController = new BooksController(injection);
+
+
+//injections:
+import IEcrypt from '../dependecies/encrypt/IEcrypt';
+import BcryptImpl from '../dependecies/encrypt/bcryptImpl'; 
+import IUserDao from '../dao/IUserDao';
+import implUserOne from '../dao/UserImpls/ImplUserOne';
+
+/*injectando a authController */
+const userDao:IUserDao = new implUserOne();
+const bcrypt:IEcrypt = new BcryptImpl();
+export const authController = new AuthController(userDao,bcrypt);
